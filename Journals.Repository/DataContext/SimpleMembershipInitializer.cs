f4b819f7521a6d6d0c6e2bb08f70899c6ccee7a5
Mirche Toshevski <mirche.toshevski@gmail.com>
2017-03-14 17:35:20 +0100

    Add project files.

---
+++ Journals.Repository/DataContext/SimpleMembershipInitializer.cs
@@ -0,0 +1,20 @@
+﻿using WebMatrix.WebData;
+
+namespace Journals.Repository.DataContext
+{
+    public class SimpleMembershipInitializer
+    {
+        public SimpleMembershipInitializer()
+        {
+            // what is this?
+            using (var context1 = new JournalsContext())
+                context1.Journals.Find(1);
+
+            using (var context = new JournalsContext())
+                context.UserProfiles.Find(1);
+
+            if (!WebSecurity.Initialized)
+                WebSecurity.InitializeDatabaseConnection("JournalsDB", "UserProfile", "UserId", "UserName", autoCreateTables: true);
+        }
+    }
+}
